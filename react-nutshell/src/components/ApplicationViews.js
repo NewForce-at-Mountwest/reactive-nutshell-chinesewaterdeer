f0ab4330a8ecd95b95c97568d88d2b8a065814e6
Calebooski <mrmeadows2018@gmail.com>
2019-10-08 11:37:35 -0400

    updated events page

--- react-nutshell/src/components/ApplicationViews.js
+++ react-nutshell/src/components/ApplicationViews.js
@@ -4,22 +4,22 @@ import React, { Component } from "react";
 import EventList from "./event/EventList";
 import EventForm from "./event/EventForm";
 import EventEditForm from "./event/EventEditForm";
-import NewsList from './news/NewsList'
-import NewsForm from './news/NewsForm'
-import NewsEditForm from './news/NewsEditForm'
-import MessageList from './messages/MessageList'
-import MessageForm from './messages/MessageForm'
-import MessageEditForm from './messages/MessageEditForm'
-import Home from './home/Home'
-import Login from './authenticate/Login'
-import PhotoList from './photos/PhotoList'
-import PhotoEditForm from './photos/PhotoEditForm'
-import PhotoForm from './photos/PhotoForm'
+import NewsList from "./news/NewsList";
+import NewsForm from "./news/NewsForm";
+import NewsEditForm from "./news/NewsEditForm";
+import MessageList from "./messages/MessageList";
+import MessageForm from "./messages/MessageForm";
+import MessageEditForm from "./messages/MessageEditForm";
+import Home from "./home/Home";
+import Login from "./authenticate/Login";
+import PhotoList from "./photos/PhotoList";
+import PhotoEditForm from "./photos/PhotoEditForm";
+import PhotoForm from "./photos/PhotoForm";
 
 // Task Imports
 import TaskList from "./task/TaskList";
 import TaskForm from "./task/TaskForm";
-import TaskEditForm from './task/TaskEditForm'
+import TaskEditForm from "./task/TaskEditForm";
 //
 import Register from "./register/Register";
 
@@ -27,8 +27,7 @@ import Register from "./register/Register";
 class ApplicationViews extends Component {
   // Check if credentials are in local storage
   // returns true/false
-  credentialAuth = () => localStorage.getItem("credentials") !== null;
-
+  credentialAuth = () => localStorage.getItem("userId") !== null;
 
   render() {
     return (
@@ -65,7 +64,6 @@ class ApplicationViews extends Component {
             );
           }}
         />
-
         <Route
           path="/events/:eventId(\d+)/edit"
           render={props => {
@@ -76,89 +74,150 @@ class ApplicationViews extends Component {
             );
           }}
         />
-
         {/* _______________________NEWS___________________________ */}
-
         <Route
           exact
           path="/news"
           render={props => {
-            return this.credentialAuth() ? <NewsList {...props} /> : <Redirect to="/" />
+            return this.credentialAuth() ? (
+              <NewsList {...props} />
+            ) : (
+              <Redirect to="/" />
+            );
           }}
         />
         <Route
           path="/news/new"
           render={props => {
-            return this.credentialAuth() ? <NewsForm {...props} /> : <Redirect to="/" />
+            return this.credentialAuth() ? (
+              <NewsForm {...props} />
+            ) : (
+              <Redirect to="/" />
+            );
           }}
         />
         <Route
           path="/news/:newId(\d+)/edit"
           render={props => {
-            return this.credentialAuth() ? <NewsEditForm {...props} /> : <Redirect to="/" />
+            return this.credentialAuth() ? (
+              <NewsEditForm {...props} />
+            ) : (
+              <Redirect to="/" />
+            );
           }}
         />
-    );
-
-
-        <Route exact path="/" render={(props) => {
-          return <Login {...props} />
-        }} />
-
+        <Route
+          exact
+          path="/"
+          render={props => {
+            return <Login {...props} />;
+          }}
+        />
         <Route
           exact
           path="/register"
           render={props => {
             return <Register {...props} />;
-          }} />
-
+          }}
+        />
         {/* Photos with auth */}
-        <Route exact path="/photos" render={(props) => {
-                    return this.credentialAuth() ? <PhotoList {...props} /> : <Redirect to="/" />
-                }} />
-                <Route exact path="/photos/new" render={(props) => {
-                       return this.credentialAuth() ? <PhotoForm {...props} /> : <Redirect to="/" />
-                }} />
-                <Route path="/photos/:photoId(\d+)/edit" render={(props) => {
-                        return this.credentialAuth() ? <PhotoEditForm {...props} /> : <Redirect to="/" />
-                }} />
-
+        <Route
+          exact
+          path="/photos"
+          render={props => {
+            return this.credentialAuth() ? (
+              <PhotoList {...props} />
+            ) : (
+              <Redirect to="/" />
+            );
+          }}
+        />
+        <Route
+          exact
+          path="/photos/new"
+          render={props => {
+            return this.credentialAuth() ? (
+              <PhotoForm {...props} />
+            ) : (
+              <Redirect to="/" />
+            );
+          }}
+        />
+        <Route
+          path="/photos/:photoId(\d+)/edit"
+          render={props => {
+            return this.credentialAuth() ? (
+              <PhotoEditForm {...props} />
+            ) : (
+              <Redirect to="/" />
+            );
+          }}
+        />
         {/* // Task Routes  */}
-
         <Route
           exact
           path="/tasks"
           render={props => {
-            return this.credentialAuth() ? <TaskList {...props} /> : <Redirect to="/" />
+            return this.credentialAuth() ? (
+              <TaskList {...props} />
+            ) : (
+              <Redirect to="/" />
+            );
           }}
         />
         <Route
           path="/tasks/new"
           render={props => {
-            return this.credentialAuth() ? <TaskForm {...props} /> : <Redirect to="/" />
+            return this.credentialAuth() ? (
+              <TaskForm {...props} />
+            ) : (
+              <Redirect to="/" />
+            );
           }}
         />
         <Route
           path="/tasks/:taskId(\d+)/edit"
           render={props => {
-            return this.credentialAuth() ? <TaskEditForm {...props} /> : <Redirect to="/" />
+            return this.credentialAuth() ? (
+              <TaskEditForm {...props} />
+            ) : (
+              <Redirect to="/" />
+            );
           }}
         />
-
-        <Route exact path="/messages" render={(props) => {
-          return this.credentialAuth() ? <MessageList {...props} /> : <Redirect to="/" />
-        }}
+        <Route
+          exact
+          path="/messages"
+          render={props => {
+            return this.credentialAuth() ? (
+              <MessageList {...props} />
+            ) : (
+              <Redirect to="/" />
+            );
+          }}
         />
-        <Route path="/messages/new" render={(props) => {
-          return this.credentialAuth() ? <MessageForm {...props} /> : <Redirect to="/" />
-        }}
+        <Route
+          path="/messages/new"
+          render={props => {
+            return this.credentialAuth() ? (
+              <MessageForm {...props} />
+            ) : (
+              <Redirect to="/" />
+            );
+          }}
         />
-        <Route path="/messages/:messageId(\d+)/edit" render={props => {
-          return this.credentialAuth() ? <MessageEditForm {...props} /> : <Redirect to="/" />
-        }}
+        <Route
+          path="/messages/:messageId(\d+)/edit"
+          render={props => {
+            return this.credentialAuth() ? (
+              <MessageEditForm {...props} />
+            ) : (
+              <Redirect to="/" />
+            );
+          }}
         />
       </React.Fragment>
-    )
+    );
   }
 }
 
